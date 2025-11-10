a4a3182c5fcb8a340de82285a98f8dc5c0489704
Tristan <tristancarter2772@gmail.com>
2025-11-10 17:21:52 -0600

    built homepage with daisyUI components

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary">
-      View Map <Icon name="tabler:map" />
-    </button>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content min-h-96 text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and the memories they hold with this simple travel log. Add locations, photos, and notes to create a journal of your travel memories.
+        </p>
+        <button class="btn btn-accent">
+          Sign In <Icon name="tabler:brand-github" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
