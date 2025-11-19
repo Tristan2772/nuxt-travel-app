bbe97d0020a500a3dbe00709e17ed25fa6163844
Tristan Carter <tristancarter2772@gmail.com>
2025-11-18 18:42:18 -0600

    form to add a location  (#61)
    * started to set up adding location form
    
    * finished form setup and protect from csrf attacks

---
+++ app/components/app/form-input.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  label: string;
+  name: string;
+  component: string;
+  type?: "text" | "textarea" | "number";
+  error?: string;
+  disabled?: boolean;
+}>();
+</script>
+
+<template>
+  <fieldset class="fieldset">
+    <legend class="fieldset-legend">
+      {{ props.label }}
+    </legend>
+    <Field
+      :as="props.component"
+      :name="props.name"
+      :type="props.type"
+      :disabled="disabled"
+      class="w-full"
+      :class="{
+        'input': !props.type || props.type === 'text' || 'number',
+        'textarea': props.type === 'textarea',
+        'input-error': props.error }"
+    />
+    <p v-if="props.error" class="label text-error">
+      {{ props.error }}
+    </p>
+  </fieldset>
+</template>
