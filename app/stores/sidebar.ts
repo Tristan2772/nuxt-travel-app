0154dd347a3a28ed3c905485b6718a28095172d7
Tristan <tristancarter2772@gmail.com>
2025-12-11 09:31:33 -0600

    added location log edit pages

--- app/stores/sidebar.ts
+++ app/stores/sidebar.ts
@@ -11,9 +11,11 @@ export type SidebarItem = {
 };
 export const useSidebarStore = defineStore("useSidebarStore", () => {
   const sidebarItems = ref<SidebarItem[]>([]);
+  const sidebarTopItems = ref<SidebarItem[]>([]);
   const loading = ref(false);
   return {
     sidebarItems,
+    sidebarTopItems,
     loading,
   };
 });
