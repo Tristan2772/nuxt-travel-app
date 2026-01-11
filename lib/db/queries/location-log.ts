33693569fadbe209a9bac768cad53932d8ec6dd4
Tristan Carter <tristancarter2772@gmail.com>
2026-01-11 12:23:45 -0600

    able to edit location logs (#85)

--- lib/db/queries/location-log.ts
+++ lib/db/queries/location-log.ts
@@ -5,21 +5,47 @@ import type { InsertLocationLog } from "../schema";
 import db from "..";
 import { locationLog } from "../schema";
 
-export async function findLocationLog(id: number, userId: number) {
+export async function findLocationLog(
+  id: number,
+  userId: number,
+) {
   const foundLog = await db.query.locationLog.findFirst({
     where: and(
       eq(locationLog.id, id),
       eq(locationLog.userId, userId),
     ),
   });
-  return foundLog;
-};
 
-export async function insertLocationLog(locationId: number, insertable: InsertLocationLog, userId: number) {
+  return foundLog;
+}
+
+export async function insertLocationLog(
+  locationId: number,
+  insertable: InsertLocationLog,
+  userId: number,
+) {
   const [inserted] = await db.insert(locationLog).values({
     ...insertable,
     locationId,
     userId,
   }).returning();
+
   return inserted;
+}
+
+export async function updateLocationLog(
+  locationLogId: number,
+  updatable: InsertLocationLog,
+  userId: number,
+) {
+  const [updated] = await db.update(locationLog).set({
+    ...updatable,
+  }).where(
+    and(
+      eq(locationLog.id, locationLogId),
+      eq(locationLog.userId, userId),
+    ),
+  ).returning();
+
+  return updated;
 }
